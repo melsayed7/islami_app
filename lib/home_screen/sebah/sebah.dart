20eebfa778474372f60bcf8a536fdacdc018cd5f
melsayed7 <mohamedelbiomy336@gmail.com>
2022-11-19 22:43:48 +0200

    add sebha screen and added code

--- lib/home_screen/sebah/sebah.dart
+++ lib/home_screen/sebah/sebah.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:islami_app/my_theme.dart';
 
-class SebahScreen extends StatelessWidget {
+class SebahScreen extends StatefulWidget {
   static const String routeName = 'sebah-screen';
+
+  @override
+  State<SebahScreen> createState() => _SebahScreenState();
+}
+
+class _SebahScreenState extends State<SebahScreen> {
+  int counter = 0;
+  int index = 0;
+  double angle = 0;
+  List<String> tasbeehat = [
+    "سبحان الله",
+    "الحمد لله",
+    "لا اله الا الله",
+    "الله اکبر",
+    "لا حول وقوة الا بالله"
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +32,25 @@ class SebahScreen extends StatelessWidget {
       Scaffold(
         body: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(40),
-              child:
-                  Center(child: Image.asset('assets/images/sebah_image.png')),
+            Stack(
+              alignment: AlignmentDirectional.topCenter,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 40, left: 80),
+                  child: Image.asset('assets/images/head_of_seb7a.png'),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 120),
+                  alignment: AlignmentDirectional.topCenter,
+                  child: Transform.rotate(
+                    angle: angle,
+                    child: Image.asset('assets/images/body_of_seb7a.png'),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
             ),
             Text(
               'عدد التسبيحات',
@@ -30,35 +61,60 @@ class SebahScreen extends StatelessWidget {
               height: 10,
             ),
             Container(
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                    color: const Color(0xffB7935F),
-                    borderRadius: BorderRadius.circular(25)),
-                child: Text(
-                  '30',
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              decoration: BoxDecoration(
+                  color: const Color(0xffB7935F),
+                  borderRadius: BorderRadius.circular(20)),
+              child: Text('$counter',
                   style: Theme.of(context)
                       .textTheme
                       .headline1
-                      ?.copyWith(fontSize: 22),
-                )),
-            const SizedBox(
-              height: 20,
+                      ?.copyWith(fontSize: 25)),
             ),
-            Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                    color: const Color(0xffB7935F),
-                    borderRadius: BorderRadius.circular(25)),
-                child: Text(
-                  'سبحان الله',
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  angle += 12;
+                  increaseCounter();
+                });
+              },
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(const Color(0xffB7935F)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ))),
+              child: Text(tasbeehat[index],
                   style: Theme.of(context)
                       .textTheme
-                      .headline1
-                      ?.copyWith(fontSize: 22, color: MyTheme.whiteColor),
-                )),
+                      .headline2
+                      ?.copyWith(color: MyTheme.whiteColor, fontSize: 27)),
+            ),
           ],
         ),
       )
     ]);
+  }
+
+  increaseCounter() {
+    if (counter < 32) {
+      counter++;
+    } else {
+      counter = 0;
+      increaseTasbehat();
+    }
+  }
+
+  increaseTasbehat() {
+    if (index < 5) {
+      index++;
+    }
+    if (index == 5) {
+      index = 0;
+    }
   }
 }
